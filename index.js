c4af6cff72d5420b38480742ebb8111c03bd0926
randomsun <17090029370@163.com>
2017-05-12 18:30:29 +0800

    截取header的部分值

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const app = express()
 
 
 app.set('port', process.env.PORT || 5000)
+app.set('trust proxy', true)
 app.use(express.static(__dirname + '/public'))
 
 
 app.get('/api/whoami', (req, res) => {
   const ipaddress = req.ip
-  const language = req.get('Accept-Language')
-  const software = req.get('User-Agent')
-  
+  const language = req.get('Accept-Language').split(',')[0]
+  const software = req.get('User-Agent').match(/\((.*;.*;.*\d)\)\s/)[1]
+
   res.json({
     ipaddress,
     language,
